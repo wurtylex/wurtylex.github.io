1fa04cb97b9b0a8831cf5a9fe0663e36e13ba22a
wurtylex <anthony.c.chang10@gmail.com>
2026-03-21 00:11:55 -0400

    cleaned up css, fixed dark mode floating button, added a way to do references

---
+++ src/pages/on-circles-and-waves/references.js
@@ -0,0 +1,32 @@
+export const references = {
+  fourier1822: {
+    author: 'Fourier, J.',
+    title: 'Théorie analytique de la chaleur',
+    year: 1822,
+    publisher: 'Firmin Didot',
+  },
+  norvig2009: {
+    author: 'Halevy, A., Norvig, P., and Pereira, F.',
+    title: 'The Unreasonable Effectiveness of Data',
+    year: 2009,
+    journal: 'IEEE Intelligent Systems',
+    volume: '24(2)',
+    pages: '8–12',
+  },
+  gibbs1899: {
+    author: 'Gibbs, J. W.',
+    title: 'Fourier Series',
+    year: 1899,
+    journal: 'Nature',
+    volume: '59',
+    pages: '606',
+  },
+  cooley1965: {
+    author: 'Cooley, J. W. and Tukey, J. W.',
+    title: 'An Algorithm for the Machine Calculation of Complex Fourier Series',
+    year: 1965,
+    journal: 'Mathematics of Computation',
+    volume: '19(90)',
+    pages: '297–301',
+  },
+};
